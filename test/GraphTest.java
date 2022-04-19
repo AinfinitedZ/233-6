35ab3931177c71d159bc344208678a5a3ce6c5e1
AinfinitedZ <43518696+AinfinitedZ@users.noreply.github.com>
2022-04-19 15:13:05 -0400

    11% complete

---
+++ test/GraphTest.java
@@ -0,0 +1,58 @@
+import org.junit.Test;
+
+public class GraphTest {
+
+	@Test
+	public void testVertex() throws Exception {
+
+	}
+
+	@Test
+	public void testEquals() throws Exception {
+
+	}
+
+	@Test
+	public void testCompareTo() throws Exception {
+
+	}
+
+	@Test
+	public void testGraph() throws Exception {
+
+	}
+
+	@Test
+	public void testAddNode() throws Exception {
+
+	}
+
+
+	@Test
+	public void testAddEdge() throws Exception {
+
+	}
+
+
+	@Test
+	public void testRemoveNode() throws Exception {
+
+	}
+
+
+	@Test
+	public void testPrintGraph() throws Exception {
+
+	}
+
+	@Test
+	public void testSetMaximum() throws Exception {
+
+	}
+
+	@Test
+	public void testRead() throws Exception {
+
+	}
+
+}
